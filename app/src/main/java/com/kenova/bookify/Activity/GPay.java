d273280dd3e061aa2763575214325106d88f233f
Aaron <aaronrono42@gmail.com>
2020-04-25 14:23:36 +0300

    Added STK Push Activity

---
+++ app/src/main/java/com/kenova/bookify/Activity/GPay.java
@@ -0,0 +1,13 @@
+package com.kenova.bookify.Activity;
+
+public class GPay {
+    private static final String TAG = "kenova.inappbilling";            // Here you can write anything you like! For example: obama.white.house.billing
+    private static final String TOKEN = "kenova.inappbilling";          // Here you can write anything you like! For example: obama.white.house.billing
+
+    // See documentation!
+
+    static final String ITEM_SKU_1 = "com.kenova.iap1";          // Change to: yourdomain.com.iap1
+    static final String ITEM_SKU_2 = "com.kenova.iap2";          // Change to: yourdomain.com.iap2
+    static final String ITEM_SKU_3 = "com.kenova.iap3";          // Change to: yourdomain.com.iap3
+    static final String ITEM_SKU_4 = "android.test.purchased";          // Not used. For testing
+}
