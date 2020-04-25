f5911499cfa49a6a6112c41a7b9e24819f983cbd
Aaron <aaronrono42@gmail.com>
2020-04-25 16:07:42 +0300

    Added STK Push

--- app/src/main/java/com/kenova/bookify/Mpesa/Payment.java
+++ app/src/main/java/com/kenova/bookify/Mpesa/Payment.java
@@ -1,169 +1,99 @@
 package com.kenova.bookify.Mpesa;
 
-import android.app.ProgressDialog;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ProgressBar;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-
-import com.afollestad.materialdialogs.GravityEnum;
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.afollestad.materialdialogs.MaterialDialog.ButtonCallback;
-import com.bdhobare.mpesa.Mode;
-import com.bdhobare.mpesa.Mpesa;
-import com.bdhobare.mpesa.interfaces.AuthListener;
-import com.bdhobare.mpesa.interfaces.MpesaListener;
-import com.bdhobare.mpesa.models.STKPush;
-import com.bdhobare.mpesa.models.STKPush.Builder;
-import com.bdhobare.mpesa.utils.Pair;
+import com.androidstudy.daraja.Daraja;
+import com.androidstudy.daraja.DarajaListener;
+import com.androidstudy.daraja.model.AccessToken;
+import com.androidstudy.daraja.model.LNMExpress;
+import com.androidstudy.daraja.model.LNMResult;
+import com.androidstudy.daraja.util.TransactionType;
 import com.kenova.bookify.R;
 
-public class Payment extends AppCompatActivity implements AuthListener, MpesaListener {
-    //TODO: Replace these values from
-    public static final String BUSINESS_SHORT_CODE = "YOUR_SHORTCODE";
-    public static final String PASSKEY = "YOUR_PASSKEY";
-    public static final String CONSUMER_KEY = "YOUR_KEY";
-    public static final String CONSUMER_SECRET = "YOUR_SECRET";
-    public static final String CALLBACK_URL = "YOUR_CALLBACK_URL";
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-
-    public static final String  NOTIFICATION = "PushNotification";
-    public static final String SHARED_PREFERENCES = "com.kenova.bookify.Mpesa";
-
-    Button pay;
-    ProgressDialog dialog;
-    EditText phone;
-    EditText amount;
-
-    private BroadcastReceiver mRegistrationBroadcastReceiver;
+public class Payment extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.phone)
+    EditText mNumber;
+    @BindView(R.id.BookPrice)
+    EditText mAmount;
+    @BindView(R.id.button)
+    Button mButton;
+    @BindView(R.id.progressBar)
+    ProgressBar mLoad;
+    private Daraja daraja;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stkpush);
-        pay = (Button)findViewById(R.id.pay);
-        phone = (EditText)findViewById(R.id.phone);
-        amount = (EditText)findViewById(R.id.amount);
-        Mpesa.with(this, CONSUMER_KEY, CONSUMER_SECRET);
-        dialog = new ProgressDialog(this);
-        dialog.setMessage("Processing");
-        dialog.setIndeterminate(true);
-
-        pay.setOnClickListener(new OnClickListener() {
+        ButterKnife.bind(this);
+        mButton.setOnClickListener(this);
+        daraja = Daraja.with("lpRnSFJyeMPnJ90yO0pOG8grwRuDm3il", "0OTXfudvr2xzLb1Y", new DarajaListener<AccessToken>() {
             @Override
-            public void onClick(View view) {
-                String p = phone.getText().toString();
-                int a = Integer.valueOf(amount.getText().toString());
-                if (p.isEmpty()){
-                    phone.setError("Enter phone.");
-                    return;
-                }
-                pay(p, a);
+            public void onResult(AccessToken accessToken) {
+                Log.i(com.kenova.bookify.Mpesa.Payment.this.getClass().getSimpleName(), accessToken.getAccess_token());
+            }
+
+            @Override
+            public void onError(String error) {
+                Log.e(com.kenova.bookify.Mpesa.Payment.this.getClass().getSimpleName(), error);
+
             }
         });
+    }
 
-        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (intent.getAction().equals(NOTIFICATION)) {
-                    String title = intent.getStringExtra("title");
-                    String message = intent.getStringExtra("message");
-                    int code = intent.getIntExtra("code", 0);
-                    showDialog(title, message, code);
+    @Override
+    public void onClick(View v) {
+        if (v == mButton) {
+            String phonenumber = mNumber.getText().toString().trim(); //collect the number from the user's input
+            String Amount = mAmount.getText().toString().trim(); // amount privided by the user
 
-                }
+            //check validity of a number
+            if (phonenumber.isEmpty() || phonenumber.length() != 10) {
+                mNumber.setError("Invalid number");
+                return;
             }
-        };
-    }
+            //check validity of a number
+            else if (Integer.valueOf(Amount) <= 0) {
+                mAmount.setError("Amount should be more than 0");
+                return;
+            }
+            mLoad.setVisibility(View.VISIBLE);
+            LNMExpress lnmExpress = new LNMExpress(
+                    "174379",
+                    "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",  //https://developer.safaricom.co.ke/test_credentials
+                    TransactionType.CustomerPayBillOnline,
+                    Amount,
+                    "0700000000",
+                    "174379",
+                    phonenumber,
+                    "http://mycallbackurl.com/checkout.php",
+                    "001ABC",
+                    "Book Purchase from Bookify"
+            );
+            daraja.requestMPESAExpress(lnmExpress,
+                    new DarajaListener<LNMResult>() {
+                        @Override
+                        public void onResult(@NonNull LNMResult lnmResult) {
+                            Log.i(com.kenova.bookify.Mpesa.Payment.this.getClass().getSimpleName(), lnmResult.ResponseDescription);
+                        }
 
-    @Override
-    public void onAuthError(Pair<Integer, String> result) {
-        Log.e("Error", result.message);
-    }
-
-    @Override
-    public void onAuthSuccess() {
-
-        //TODO make payment
-        pay.setEnabled(true);
-    }
-    private void pay(String phone, int amount){
-        dialog.show();
-        STKPush.Builder builder = new Builder(BUSINESS_SHORT_CODE, PASSKEY, amount,BUSINESS_SHORT_CODE, phone);
-
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
-        String token = sharedPreferences.getString("InstanceID", "");
-
-        builder.setFirebaseRegID(token);
-        STKPush push = builder.build();
-
-
-
-        Mpesa.getInstance().pay(this, push);
-
-    }
-    private void showDialog(String title, String message,int code){
-        MaterialDialog dialog = new MaterialDialog.Builder(this)
-                .title(title)
-                .titleGravity(GravityEnum.CENTER)
-                .customView(R.layout.success_dialog, true)
-                .positiveText("OK")
-                .cancelable(false)
-                .widgetColorRes(R.color.colorPrimary)
-                .callback(new ButtonCallback() {
-                    @Override
-                    public void onPositive(MaterialDialog dialog) {
-                        super.onPositive(dialog);
-                        dialog.dismiss();
-                        finish();
+                        @Override
+                        public void onError(String error) {
+                            Log.i(com.kenova.bookify.Mpesa.Payment.this.getClass().getSimpleName(), error);
+                        }
                     }
-                })
-                .build();
-        View view=dialog.getCustomView();
-        TextView messageText = (TextView)view.findViewById(R.id.message);
-        ImageView imageView = (ImageView)view.findViewById(R.id.success);
-        if (code != 0){
-            imageView.setVisibility(View.GONE);
+            );
         }
-        messageText.setText(message);
-        dialog.show();
-    }
-
-    @Override
-    public void onMpesaError(Pair<Integer, String> result) {
-        dialog.hide();
-        Toast.makeText(this, result.message, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onMpesaSuccess(String MerchantRequestID, String CheckoutRequestID, String CustomerMessage) {
-        dialog.hide();
-        Toast.makeText(this, CustomerMessage, Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(NOTIFICATION));
-
-    }
-    @Override
-    protected void onPause() {
-       LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
-        super.onPause();
     }
 }
